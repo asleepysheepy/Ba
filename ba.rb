1c8c0c56e11e7d229820533fb085f8004e6f5412
Katie <katie@flutterflies.net>
2018-04-13 20:39:59 -0400

    Use custom emojis for nya and awoo

--- ba.rb
+++ ba.rb
@@ -6,10 +6,14 @@ module Ba
   bot = Discordrb::Bot.new token: ENV['TOKEN']
   INVITE_URL = "#{bot.invite_url}&permissions=64".freeze
 
+  AWOO_EMOJI = 'awoo:434500209012375553'.freeze
+  NYA_EMOJI = 'nya:434511854505558019'.freeze
+  BA_EMOJI = '🐑'.freeze
+
   bot.message do |event|
-    event.message.react('🐑') if event.message.content.downcase =~ /\bba+\b/
-    event.message.react('🐈') if event.message.content.downcase =~ /\bnya+\b/
-    event.message.react('🐶') if event.message.content.downcase =~ /\bawo+\b/
+    event.message.react(BA_EMOJI) if event.message.content.downcase =~ /\bba+\b/
+    event.message.react(NYA_EMOJI) if event.message.content.downcase =~ /\bnya+\b/
+    event.message.react(AWOO_EMOJI) if event.message.content.downcase =~ /\bawo+\b/
   end
 
   bot.ready do |event|
