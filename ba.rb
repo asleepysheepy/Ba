b3cfacd9645af7bafd951b9aee6f4b08c0c3c52b
Emma <25013194+bovarysme@users.noreply.github.com>
2018-04-30 17:00:27 +0200

    Choo Choo (#4)

--- ba.rb
+++ ba.rb
@@ -9,11 +9,13 @@ module Ba
   AWOO_EMOJI = 'awoo:434500209012375553'.freeze
   NYA_EMOJI = 'nya:434511854505558019'.freeze
   BA_EMOJI = '🐑'.freeze
+  TRAIN_EMOJI = '🚄'.freeze
 
   bot.message do |event|
     event.message.react(BA_EMOJI) if event.message.content.downcase =~ /\bba+\b/
     event.message.react(NYA_EMOJI) if event.message.content.downcase =~ /\bnya+h?\b/
     event.message.react(AWOO_EMOJI) if event.message.content.downcase =~ /\baw(u+|o+)\b/
+    event.message.react(TRAIN_EMOJI) if event.message.content.downcase =~ /\btra+in\b/
   end
 
   bot.ready do |event|
