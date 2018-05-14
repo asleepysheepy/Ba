968b5c9331f35d20938d477a7ad5dab5cd34e38c
Katie <katie@flutterflies.net>
2018-05-13 21:42:30 -0400

    add command to see number of servers running the bot

--- lib/ba_bot.rb
+++ lib/ba_bot.rb
@@ -9,11 +9,12 @@ class BaBot
   attr_accessor :invite_url, :bot
 
   def initialize(token)
-    @bot = Discordrb::Bot.new token: token
-    @invite_url = "#{@bot.invite_url}&permissions=64"
+    @bot = Discordrb::Commands::CommandBot.new token: token, prefix: '?ba '
+    @invite_url = "#{@bot.invite_url}&permissions=2112"
 
     @bot.message { |event| message event }
     @bot.ready { |event| event.bot.game = 'Ba' }
+    @bot.command(:num_servers) { |event| puts 'run'; num_servers event }
   end
 
   def message(event)
@@ -21,5 +22,17 @@ class BaBot
     get_message_reactions(message_content).each do |emote|
       event.message.react emote
     end
+  end
+
+  def num_servers(event)
+    puts 'running'
+    unless event.author.id == 145_696_462_959_935_488
+      event << "I'm sorry #{event.author.mention}, you cannot run this command."
+      event << 'You must be the bot owner to user this command'
+      return
+    end
+
+    event << "Ba is currently running on #{@bot.servers.size} servers"
+    puts 'ran'
   end
 end
