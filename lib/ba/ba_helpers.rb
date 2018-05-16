d9002e53d4c17cae782257593725af5c135807dc
Carys <carystryhorn@gmail.com>
2018-05-16 19:03:39 -0400

    Added 🐕 to when you say arf+

--- lib/ba/ba_helpers.rb
+++ lib/ba/ba_helpers.rb
@@ -8,6 +8,7 @@ module Ba
       reactions << 'nya:434511854505558019' if message =~ /\bnya+[hn]?\b/
       reactions << 'awoo:434500209012375553' if message =~ /\baw(u+|oo+)\b/
       reactions << '🚄' if message =~ /\btra+in\b/
+	  reactions << '🐕' if message =~ /\barf+\b/
       reactions
     end
   end
