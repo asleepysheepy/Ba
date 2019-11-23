b025551e7e09f5a74c63db11ca09e8a9672bdde2
Katie Macke <katie.macke@joinroot.com>
2019-11-23 13:08:35 -0500

    add favicon

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -34,6 +34,7 @@ module.exports = {
   plugins: [
     new HtmlWebPackPlugin({
       template: './src/index.html',
+      favicon: './src/favicon.ico',
       filename: './index.html',
     }),
   ],
