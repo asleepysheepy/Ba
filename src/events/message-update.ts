13c0325e72f8ead90542f3bdf1173fae24670ab5
Katie Macke <katie@sleepysheepy.net>
2020-12-20 18:47:52 -0500

    share message response logic

--- src/events/message-update.ts
+++ src/events/message-update.ts
@@ -1,18 +1,10 @@
-import { Emote } from '../models/emote'
 import { Event } from './event'
 import { Message } from 'discord.js'
+import { MessageUtils } from '../utils/messages'
 
-const MessageUpdateEvent: Event = {
+export const MessageUpdateEvent: Event = {
   eventName: 'messageUpdate',
   handle: async (oldMessage: Message, newMessage: Message): Promise<void> => {
-    const emotes = await Emote.forToday()
-
-    emotes.forEach((emote) => {
-      if (emote.shouldReact(newMessage.content)) {
-        newMessage.react(emote.emoji)
-      }
-    })
+    MessageUtils.handleReactions(newMessage)
   },
 }
-
-export { MessageUpdateEvent }
--- src/events/message.ts
+++ src/events/message.ts
@@ -1,37 +1,11 @@
 import { Client, Message } from 'discord.js'
-import { Emote } from '../models/emote'
 import { Event } from './event'
-import { createEmbed } from '../utils/embeds'
+import { MessageUtils } from '../utils/messages'
 
-const checkMention = async (message: Message, client: Client): Promise<void> => {
-  if (!client.user) { return }
-  if (!message.guild) { return }
-  if (!message.mentions.has(client.user?.id)) { return }
-
-  const response = await createEmbed(message.author, client)
-  response.setTitle('Welcome to Ba! 🐑')
-    .setDescription('Ba is a bot the looks to spice up your server by adding fun reactions to your messages.')
-    .addField('Github (PRs encouraged)', 'https://github.com/asleepysheepy/Ba')
-    .addField('Invite Link', client.generateInvite({ permissions: ['SEND_MESSAGES', 'ADD_REACTIONS'] }))
-  message.channel.send(response)
-}
-
-const checkReactions = async (message: Message): Promise<void> => {
-  const emotes = await Emote.forToday()
-
-  emotes.forEach((emote) => {
-    if (emote.shouldReact(message.content)) {
-      message.react(emote.emoji)
-    }
-  })
-}
-
-const MessageEvent: Event = {
+export const MessageEvent: Event = {
   eventName: 'message',
   handle: (message: Message, client: Client): void => {
-    checkMention(message, client)
-    checkReactions(message)
+    MessageUtils.handleMention(message, client)
+    MessageUtils.handleReactions(message)
   },
 }
-
-export { MessageEvent }
